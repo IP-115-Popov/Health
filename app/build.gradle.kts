253fb1997344c1ff0bec934ff947d7f89f5a74ae
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-10-09 14:37:46 +0700

    Updated layers dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":data"))
+    implementation(project(":domain"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
