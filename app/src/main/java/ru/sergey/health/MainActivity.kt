44fc4402eaf820cdc352a8d8b5a98b75ed482794
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-10-09 14:08:57 +0700

    Initial commit

---
+++ app/src/main/java/ru/sergey/health/MainActivity.kt
@@ -0,0 +1,72 @@
+package ru.sergey.health
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import ru.sergey.health.ui.theme.HealthTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+           /*HealthTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }*/
+        }
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun test()
+{
+    Column(modifier = Modifier.background(Color.Red).fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.End) {
+        Text(text = "Hello METANIT.COM!", fontSize = 28.sp)
+        Text(text = "Hello METANIT.COM!", fontSize = 28.sp)
+        Text(text = "Hello METANIT.COM!", fontSize = 28.sp)
+        Row(modifier = Modifier.background(Color.Gray).fillMaxHeight(0.5f)) {
+            Text(text = "Hello ", fontSize = 28.sp)
+            Text(text = "Hello ", fontSize = 28.sp)
+            Text(text = "Hello ", fontSize = 28.sp)
+        }
+    }
+
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+@Composable
+fun GreetingPreview() {
+    Text(text = "Hello METANIT.COM!", fontSize = 28.sp)
+    //HealthTheme {
+    ////    Greeting("Android")
+    //}
+}
