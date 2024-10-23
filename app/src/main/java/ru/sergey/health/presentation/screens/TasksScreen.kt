9697267e494d85ddf00af62f71b29443508fa995
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-10-23 16:37:14 +0700

    Update TasksScreen

--- app/src/main/java/ru/sergey/health/presentation/screens/TasksScreen.kt
+++ app/src/main/java/ru/sergey/health/presentation/screens/TasksScreen.kt
@@ -24,12 +24,15 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,10 +40,14 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import ru.sergey.health.R
+import ru.sergey.health.presentation.theme.ui.Pink80
 import ru.sergey.health.presentation.theme.ui.Purple80
 import kotlin.random.Random
 
@@ -56,15 +63,89 @@ fun TasksScreen(vm : TasksViewModel) {
         verticalItemSpacing = 8.dp
     ) {
         items(vm.tasks) {item ->
-            TaskView(item, vm)
+            TaskView(item)//, vm)
         }
     }
 }
-//@Preview(showSystemUi = true)
+@Preview(showSystemUi = true)
 @Composable
-fun TaskView(task: Task = Task(1,"Бег","Бегать каждый день по 10 км", 0, 100, "Дней"), vm : TasksViewModel)
+fun TaskView(task: Task = Task(1,"Бег","Бегать каждый день по 10 км", 0, 100, "Дней"))//, vm : TasksViewModel)
 {
-    Surface(modifier = Modifier
+    val textModifier = Modifier.padding(5.dp)
+
+    ConstraintLayout(
+        modifier = Modifier
+            .background(
+                Color(
+                    Random.nextInt(255),
+                    Random.nextInt(255),
+                    Random.nextInt(255),
+                    255
+                ), RoundedCornerShape(10.dp)
+            )
+            .padding(10.dp)
+            .height(Random.nextInt(142, 200).dp)
+            //
+            .fillMaxWidth()
+    ) {
+        val (tvTitle, tvDescription, tvPoint, bthEdit, dthAddPoint) = createRefs()
+
+        Text(text = task.title, textModifier.then(Modifier.constrainAs(tvTitle){
+            top.linkTo(parent.top)
+            start.linkTo(parent.start, margin = 20.dp)
+        }))
+        Text(text = task.description,
+            textModifier.then(Modifier.constrainAs(tvDescription){
+            top.linkTo(bthEdit.bottom, margin = 12.dp)
+            start.linkTo(parent.start, margin = 20.dp)
+            end.linkTo(bthEdit.start, margin = 8.dp)
+            }),
+            maxLines = 3, // Установите максимальное количество строк
+            overflow = TextOverflow.Ellipsis // Если текст превышает, будет добавлено
+        )
+        Text(text = task.points.toString() + "/" + task.targetPoints + "   " + task.measureUnit, textModifier.then(Modifier.constrainAs(tvPoint){
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start, margin = 20.dp)
+        }))
+
+        IconButton(onClick = {
+            //vm.addPointsToTask(task.id)
+            //vm.updateTasks()
+        },
+            modifier = Modifier
+                .size(20.dp)
+                .background(Pink80, shape = CircleShape)
+                .constrainAs(bthEdit){
+                top.linkTo(parent.top, margin = 10.dp)
+                end.linkTo(parent.end, margin = 10.dp)
+            }
+        ){
+            Image(
+                imageVector = Icons.Filled.Create,
+                contentDescription = "Значок редактирования задачи",
+            )
+        }
+        IconButton(onClick = {
+            //vm.addPointsToTask(task.id)
+            //vm.updateTasks()
+        },
+            modifier = Modifier
+                .size(20.dp)
+                .background(Pink80, shape = CircleShape)
+                .constrainAs(dthAddPoint){
+                bottom.linkTo(parent.bottom, margin = 10.dp)
+                end.linkTo(parent.end, margin = 10.dp)
+            }
+        ){
+            Image(
+                imageVector = Icons.Filled.Add,
+                contentDescription = "Значок редактирования задачи",
+            )
+        }
+
+
+    }
+    /*Surface(modifier = Modifier
         .shadow(5.dp)
         .clip(RoundedCornerShape(10.dp))) {
         Column(
@@ -84,10 +165,15 @@ fun TaskView(task: Task = Task(1,"Бег","Бегать каждый день п
         ) {
             val textModifier = Modifier.padding(5.dp)
             Row(
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(text = task.title, textModifier)
-                Button(onClick = {},
+                IconButton(onClick = {},
+                    modifier = Modifier
+                        .size(20.dp)
+                        .background(Pink80, shape = CircleShape)
                 ){
                     Image(
                         imageVector = Icons.Filled.Create,
@@ -97,23 +183,28 @@ fun TaskView(task: Task = Task(1,"Бег","Бегать каждый день п
             }
             Text(text = task.description, textModifier)
             Row(
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(text = task.points.toString()+" / "+task.targetPoints.toString(), textModifier)
-                Text(text = task.measureUnit)
-                Spacer(modifier = Modifier.width(10.dp))
-                Button(onClick = {
-                    vm.addPointsToTask(task.id)
-                    vm.updateTasks()
+                Text(text = task.points.toString() + "/" + task.targetPoints + "   " + task.measureUnit, textModifier)
+
+                IconButton(onClick = {
+                    //vm.addPointsToTask(task.id)
+                    //vm.updateTasks()
                 },
+                    modifier = Modifier
+                        .size(20.dp)
+                        .background(Pink80, shape = CircleShape)
                 ){
                     Image(
                         imageVector = Icons.Filled.Add,
                         contentDescription = "Значок увеличения очков",
                     )
                 }
+                Spacer(modifier = Modifier.width(10.dp))
             }
 
         }
-    }
+    }*/
 }
