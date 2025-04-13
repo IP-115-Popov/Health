a8177151d89243eebe30ea67d94c0633b5924502
попов Сергей <serzh.serp123.popov@mail.ru>
2025-04-13 12:03:06 +0700

    Ref вырезал тестовые библиотеки

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -51,9 +51,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(libs.androidx.datastore)
 
